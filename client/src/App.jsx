631ced4b4e820273eb3accd7efa31ca044f6b36c
harshitclub <harshitclub@gmail.com>
2023-05-04 16:32:43 +0530

    app final

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,10 +3,25 @@ import sqlServer from "./assets/sql-server.png";
 import { useState } from "react";
 
 export default function App() {
-  const [userPrompt, setUserPrompt] = useState("");
-  const onSubmit = (e) => {
+  const [queryDescription, setQueryDescription] = useState("");
+  const [sqlQuery, setSqlQuery] = useState("");
+  const onSubmit = async (e) => {
     e.preventDefault();
-    console.log("Form Submit", userPrompt);
+    const query = await generateQuery();
+    setSqlQuery(query);
+  };
+
+  const generateQuery = async () => {
+    const response = await fetch("http://localhost:4000/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ queryDescription: queryDescription }),
+    });
+
+    const data = await response.json();
+    return data.sqlQuery.trim();
   };
   return (
     <main className={styles.main}>
@@ -17,11 +32,12 @@ export default function App() {
           type="text"
           name="query-description"
           placeholder="Describe Your Query"
-          value={userPrompt}
-          onChange={(e) => setUserPrompt(e.target.value)}
+          value={queryDescription}
+          onChange={(e) => setQueryDescription(e.target.value)}
         />
         <input type="submit" value="Generate Query" />
       </form>
+      <pre>{sqlQuery}</pre>
     </main>
   );
 }
--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 import express from "express";
 import cors from "cors";
+import generate from "./generate.js";
 
 const app = express();
+
+app.use(express.json());
 
 app.use(
   cors({
@@ -13,6 +16,18 @@ const port = 4000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/generate", async (req, res) => {
+  const { queryDescription } = req.body;
+
+  try {
+    const sqlQuery = await generate(queryDescription);
+    res.json({ sqlQuery });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error!");
+  }
 });
 
 app.listen(port, () => {
